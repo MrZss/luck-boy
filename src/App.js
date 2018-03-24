62abf50a6b4b7d193437cbdb8f737332d755cd17
MrZss <735353456@qq.com>
2018-03-24 19:34:09 +0800

    初始化

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './css/index.css';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="wrap">
+        123
+      </div>
+    );
+  }
+}
+
+
